1f07db0b5561858c8ce537eb6fe26146de5f088e
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-05-31 17:52:55 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,17 +1,18 @@
-// function post() {
-//   const submit = document.getElementById("submit");
-//   submit.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const form = document.getElementById("form");
-//     const formData = new FormData(form);
-//     const XHR = new XMLHttpRequest();
-//     XHR.open("POST", "/posts", true);
-//     XHR.responseType = "json";
-//     XHR.send(formData);
-//   });
-// };
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
 
-// window.addEventListener('load', post);
+
 
 function post() {
   const submit = document.getElementById("submit");
@@ -23,6 +24,16 @@ function post() {
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
